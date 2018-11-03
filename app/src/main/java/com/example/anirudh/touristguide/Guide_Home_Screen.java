58ca2b4d22ceb83c3fad6447c859136712389596
Anirudh <raghaavan3@gmail.com>
2018-11-02 22:37:11 -0700

    firebase integration

--- app/src/main/java/com/example/anirudh/touristguide/Guide_Home_Screen.java
+++ app/src/main/java/com/example/anirudh/touristguide/Guide_Home_Screen.java
@@ -1,7 +1,14 @@
 package com.example.anirudh.touristguide;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Guide_Home_Screen extends AppCompatActivity {
 
@@ -10,4 +17,6 @@ public class Guide_Home_Screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide__home__screen);
     }
+
+
 }
